5293f8bbb43f5184efbb28a9117689fc0a49bc6c
Pedro Pinho <rihor1000@gmail.com>
2019-08-08 22:45:57 -0300

    Adicionando espaço na string bugada

--- server/src/bootstrap.js
+++ server/src/bootstrap.js
@@ -3,5 +3,5 @@ const dotenv = require('dotenv');
 dotenv.config({
   // coloquei um espaço no fim do test de propósito
   // não estava identificando a string sem o espaço
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+  path: process.env.NODE_ENV === 'test ' ? '.env.test' : '.env',
 });

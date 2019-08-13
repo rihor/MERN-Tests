80c9f13d19c476164d7510cb40fcc36c9f93cae6
Pedro Pinho <rihor1000@gmail.com>
2019-08-13 16:57:13 -0300

    Adicionados tests de dispatch e selector

--- web/src/__tests__/components/TechList.test.js
+++ web/src/__tests__/components/TechList.test.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
+import { addTech } from "~/store/modules/techs/actions";
+
+jest.mock('react-redux');
 
 describe('TechList component', () => {
-  it('should be able to add new tech', () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
+  it('should render tech list', () => {
+    // mock de useSelector
+    useSelector.mockImplementation(callback => callback({
+      techs: ['Node.js', 'ReactJS']
+    }));
 
-    debug();
-
-    fireEvent.click(getByText('Adicionar'));
-
-    debug();
+    const { getByText, getByTestId } = render(<TechList />);
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    // mock de useDispatch
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
   });
 });
--- web/src/components/TechList/index.js
+++ web/src/components/TechList/index.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { addTech } from "../../store/modules/techs/actions";
 
 export default function TechList() {
-  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const dispatch = useDispatch();
+  const techs = useSelector(state => state.techs);
 
   function handleAddTech() {
-    setTechs([...techs, 'Node.js']);
+    dispatch(addTech(newTech));
+    setNewTech('');
   }
 
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
       <ul data-testid="tech-list">
         {techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
       <button onClick={handleAddTech}>Adicionar</button>
-    </div>
+    </form>
   );
 }
